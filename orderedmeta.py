de11ec0c2f63eceae162c043017951783bbba91a
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-21 18:59:39 +0300

    Initial import

---
+++ orderedmeta.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class Descriptor:
+    pass
+
+
+class Validate:
+    pass
+
+
+class OrderedMeta(type):
+    @classmethod
+    def __prepare__(cls, bases, **kwds):
+        pass
+
+
+if __name__ == "__main__":
+    pass
