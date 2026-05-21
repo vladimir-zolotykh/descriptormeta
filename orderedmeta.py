8a69d90e42a00b3b1d8538d6f7d50bce02db2788
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-21 20:49:57 +0300

    Add Validator, OneOf, Drawer, Lot, main guard

--- orderedmeta.py
+++ orderedmeta.py
@@ -1,21 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from abc import ABC, abstractmethod
+from collections import OrderedDict
 
 
-class Descriptor:
-    pass
+class Validator(ABC):
+    def __set_name__(self, owner, name):
+        self._name = name
 
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        return instance.__dict__[self._name]
 
-class Validate:
-    pass
+    def __set__(self, instance, value):
+        self.validate(value)
+        instance.__dict__[self._name] = value
 
-
-class OrderedMeta(type):
-    @classmethod
-    def __prepare__(cls, bases, **kwds):
+    @abstractmethod
+    def validate(self, value):
         pass
 
 
+class OneOf(Validator):
+    def __init__(self, *choices):
+        self.choices = choices
+
+    def validate(self, value):
+        if value not in self.choices:
+            raise ValueError(f"{value!r} must be one of {self.choices}")
+
+
+class OrderedMeta(type):
+    def __prepare__(clsname, bases, **kwds):
+        return OrderedDict()
+
+
+class Drawer(metaclass=OrderedMeta):
+    _fields = []
+
+
+class Lot(Drawer):
+    kind = OneOf("metal", "wood", "plastic")
+
+    def __init__(self, kind):
+        self.kind = kind
+
+
 if __name__ == "__main__":
-    pass
+    lot1 = Lot("metal")
+    print(lot1.kind)
+    try:
+        lot2 = Lot("paper")
+    except ValueError as exc:
+        print(str(exc))
